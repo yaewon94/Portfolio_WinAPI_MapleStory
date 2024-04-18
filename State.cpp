938aa000082022ebc867c2d8fca92aba7c67ad17
yaewon94 <yaewon94@naver.com>
2024-04-18 14:25:21 +0900

    State : 상태 지속시간, 경과시간 필드 추가

--- State.cpp
+++ State.cpp
@@ -1,14 +1,19 @@
 #include "PCH.h"
 #include "State.h"
+#include "FSM.h"
+#include "TimeManager.h"
 
 // 생성자
-State::State(OBJECT_STATE type, float coolDown) : type(type), CoolDown(coolDown), fsm(nullptr)
+State::State(OBJECT_STATE type, float coolDown, float Duration) 
+	: type(type), CoolDown(coolDown), Duration(Duration), fsm(nullptr)
 {
 	// fsm 초기화는 FSM::AddState()에서 담당
 }
 
 // 복사 생성자
-State::State(const State& origin) : type(origin.type), CoolDown(origin.CoolDown), fsm(nullptr)
+State::State(const State& origin) 
+	: type(origin.type), CoolDown(origin.CoolDown), Duration(origin.Duration)
+	, fsm(nullptr)
 {
 	// FSM의 복사 생성자에서 fsm 초기화
 }
@@ -17,4 +22,17 @@ State::State(const State& origin) : type(origin.type), CoolDown(origin.CoolDown)
 State::~State()
 {
 	fsm = nullptr;
+}
+
+// 매 프레임마다 호출
+void State::Stay()
+{
+	// 상태 지속시간이 존재할 경우에만, 경과시간 누적
+	if (Duration > 0.f)
+	{
+		time = TimeManager::GetInstance().GetDeltaTime();
+
+		// 지속시간을 초과한 경우, 디폴트 상태로 전환
+		if (time >= Duration) fsm->ChangeState(fsm->GetDefaultState());
+	}
 }
--- State.h
+++ State.h
@@ -13,6 +13,8 @@ private:
 	FSM* fsm;
 	OBJECT_STATE type;
 	const float CoolDown;		// 해당 상태 종료 후, 다시 해당 상태가 되기 위해 필요한 쿨타임
+	const float Duration;		// 상태 지속시간
+	float time = 0.f;			// 상태가 시작된 후 경과한 시간
 
 private:
 	void SetFsm(FSM& fsm) { this->fsm = &fsm; }
@@ -21,7 +23,7 @@ protected:
 	float exitTime = 0.f;		// 상태 종료 시간
 
 protected:
-	State(OBJECT_STATE type, float coolDown=0.f);
+	State(OBJECT_STATE type, float coolDown=0.f, float Duration=0.f);
 	State(const State& origin);
 	~State();
 	virtual State* Clone() = 0;
@@ -31,6 +33,6 @@ protected:
 	virtual void Init() {}
 
 	virtual void Enter() = 0;
-	virtual void Stay() = 0;
+	virtual void Stay();
 	virtual void Exit() = 0;
 };
